b029a85c79136c9896eb2e94594f81ba85a8a06c
Antonio Woods <antonio95woods@gmail.com>
2019-05-25 23:36:30 -0400

    Implement Kallie's feature requests

--- project_assigner.py
+++ project_assigner.py
@@ -9,8 +9,7 @@ from munkres.munkres import Munkres, print_matrix, DISALLOWED
 valid_data = True
 
 def main():
-    projects_loc, project_picks_loc, manager_picks_loc = gather_params()
-
+    projects_loc, project_picks_loc = gather_params()
     projects = ProjectDirectory(projects_loc)
     project_picks = AssociatePicksDirectory(project_picks_loc, projects)
     if valid_data:
@@ -21,8 +20,14 @@ def main():
                 "Such issues will be prefaced with \"FixMe\" in the output above.", bcolors.FAIL)
 
 def gather_params():
-    #TODO add param validation
-    return sys.argv[1], sys.argv[2], None
+    if len(sys.argv) == 3:
+        proj = sys.argv[1]
+        pick = sys.argv[2]
+    else:
+        proj = "Projects.csv"
+        pick = "Preferences.csv"
+    cprint(f"\nUsing input files: \n\t{bcolors.OKBLUE}Projects = {proj}\n\tPicks = {pick}\n", bcolors.BOLD)
+    return proj, pick
 
 def compute_assignments(project_picks):
     cprint("\nCasting Hungarian magic\n", bcolors.BOLD)
@@ -30,12 +35,6 @@ def compute_assignments(project_picks):
     m = Munkres()
     assignments = m.compute(matrix)
     print_matrix(matrix)
-    # total = 0
-    # for row, column in assignments:
-    #     value = matrix[row][column]
-    #     total += value
-    #     print('(%d, %d) -> %d' % (row, column, value))
-    #     print('total profit=%d' % total)
     return assignments
 
 def output_assignments(assignments, projects, project_picks):
@@ -46,12 +45,11 @@ def output_assignments(assignments, projects, project_picks):
     for associateidx, vacancyidx in assignments:
         assignments_out_console.append([project_picks.picks_by_idx[associateidx].associate_name, "->", 
             projects.projects_by_vacancy_number[vacancyidx].name, 
-            "(pick #{})".format(project_picks.cost_matrix[associateidx][vacancyidx])])
+            f"(pick #{project_picks.cost_matrix[associateidx][vacancyidx]})"])
 
         assignments_out_file.append([project_picks.picks_by_idx[associateidx].associate_name, 
             project_picks.picks_by_idx[associateidx].associate_id,
             projects.projects_by_vacancy_number[vacancyidx].name])
-
 
     #Print the assignments to console
     printcols(assignments_out_console, color=bcolors.OKGREEN)
@@ -61,8 +59,7 @@ def output_assignments(assignments, projects, project_picks):
     with open(out_file_name, 'w') as file:
         writer = csv.writer(file)
         writer.writerows(assignments_out_file)
-    cprint("\nSuccesfully wrote assignments to: {}".format(out_file_name), bcolors.OKGREEN)
-
+    cprint(f"\nSuccesfully wrote assignments to: {out_file_name}", bcolors.OKGREEN)
 
 
 class ProjectDirectory:
@@ -89,32 +86,34 @@ class ProjectDirectory:
             try:
                 reader = csv.reader(file)
                 for rowidx, row in enumerate(reader):
+                    if rowidx == 0 and row[0] == "Project Name":
+                        #skip first row with column names
+                        continue
                     name = row[0]
                     vacancies = row[1]
                     tech = row[2]
                     if not name:
-                        errs+=cstring("\tFixMe: (row {}) No name specified for project.\n".format(rowidx + 1), bcolors.FAIL)
+                        errs+=cstring(f"\tFixMe: (row {rowidx + 1}) No name specified for project.\n", bcolors.FAIL)
                         valid_data = False
                     else:
                         name.replace('\\ufeff', '')
                     if not vacancies:
-                        wrns+=cstring("\tWrn: (row {}) No number of vacancies specified for project. Assuming 1 free spot.\n".format(rowidx + 1), bcolors.WARNING)
+                        wrns+=cstring(f"\tWrn: (row {rowidx + 1}) No number of vacancies specified for project. Assuming 1 free spot.\n", bcolors.WARNING)
                         vacancies = 1
                     if not vacancies.isdigit():
-                        wrns+=cstring("\tWrn: (row {}) Number of vacancies for project is not a number.  Assuming 1 free spot.\n".format(rowidx + 1), bcolors.WARNING)
+                        wrns+=cstring(f"\tWrn: (row {rowidx + 1}) Number of vacancies for project is not a number.  Assuming 1 free spot.\n", bcolors.WARNING)
                         vacancies = 1
                     else:
                        vacancies = int(vacancies)
                     if all( not t for t in tech ):
-                        wrns+=cstring("\tWrn: (row {}) Project has no tech specified.".format(rowidx + 1), bcolors.WARNING)
+                        wrns+=cstring(f"\tWrn: (row {rowidx + 1}) Project has no tech specified.", bcolors.WARNING)
                     new_project = self.Project(rowidx, name, vacancies, tech)
                     projects_by_idx.append(new_project)
                     projects_by_name[new_project.name.lower()] = new_project
                     projects_by_vacancy_number.extend([new_project] * new_project.vacancies)
-                    projs_out.append([new_project.name+":", "vacancies={}".format(new_project.vacancies), "tech={}".format(new_project.tech)])
-                    #cprint("\t'{}': vacancies={} tech={}".format(new_project.name, new_project.vacancies, new_project.tech), bcolors.OKBLUE)
+                    projs_out.append([new_project.name+":", f"vacancies={new_project.vacancies}", f"tech={new_project.tech}"])
             except csv.Error as e:
-                sys.exit('file {}, line {}: {}'.format(projects_loc, reader.line_num, e))
+                sys.exit(f'file {projects_loc}, line {reader.line_num}: {e}')
             printcols(projs_out)
             print(wrns+errs)
         return projects_by_name, projects_by_idx, projects_by_vacancy_number
@@ -145,13 +144,16 @@ class AssociatePicksDirectory:
             try:
                 reader = csv.reader(file)
                 for rowidx, row in enumerate(reader):
+                    if rowidx == 0 and row[0] == "Associate Name":
+                        #skip first row with column names
+                        continue
                     associate_name = row[0]
                     associate_id = row[1].upper()
                     picks = []
                     if not associate_name:
-                        wrns+=cstring("Wrn: (row {}) Associate is missing a name.\n".format(rowidx + 1), bcolors.WARNING)
+                        wrns+=cstring(f"\tWrn: (row {rowidx + 1}) Associate is missing a name.\n", bcolors.WARNING)
                     if not associate_id:
-                        wrns+=cstring("\tFixMe: (row {}) Associate is missing an associate id.\n".format(rowidx + 1), bcolors.FAIL)
+                        wrns+=cstring(f"\tFixMe: (row {rowidx + 1}) Associate is missing an associate id.\n", bcolors.FAIL)
                         valid_data = False
                     for pick in row[2:]:
                         if not pick:
@@ -159,25 +161,24 @@ class AssociatePicksDirectory:
                         if pick.isdigit():
                             pickidx = int(pick)
                             if pickidx < 0 or pickidx >= len(projects.projects_by_idx):
-                                errs+=cstring("\tFixMe: (row {}) Project index is out of bounds.\n".format(pickidx + 1), bcolors.FAIL)
+                                errs+=cstring(f"\tFixMe: (row {rowidx + 1}) Project index {pickidx} is out of bounds.\n", bcolors.FAIL)
                                 valid_data = False
                             else:
                                 picks.append(projects.projects_by_idx[pickidx])
                         else:
                             if not pick.lower() in projects.projects_by_name:
-                                errs+=cstring("\tFixMe: (row {}) couldn't find project '{}' "
+                                errs+=cstring(f"\tFixMe: (row {rowidx + 1}) couldn't find project '{pick}' "
                                     "in projects file.\n\tConsider fixing spelling "
                                     "(case insensitive) or using a project index in "
-                                    "place of the name.\n".format(rowidx + 1, pick), bcolors.FAIL)
+                                    "place of the name.\n", bcolors.FAIL)
                                 valid_data = False
                             else:
                                 picks.append(projects.projects_by_name[pick.lower()])
                     if len(picks) == 0:
-                        wrns+=cstring("\tWrn: (row {}) No picks have been chosen.\n".format(rowidx + 1), bcolors.WARNING)
+                        wrns+=cstring(f"\tWrn: (row {rowidx + 1}) No picks have been chosen.\n", bcolors.WARNING)
                     new_picks = self.AssociatePicks(rowidx, associate_name, associate_id, picks)
                     picks_by_idx.append(new_picks)
-                    picks_out.append([new_picks.associate_id, "picks={}".format(str([ p.name for p in new_picks.picks ])) ])
-                    #cprint("\t({}) {}'s picks in order: {}".format(new_picks.associate_id, new_picks.associate_name, [ p.name for p in new_picks.picks]), bcolors.OKBLUE)
+                    picks_out.append([new_picks.associate_id, f"picks={str([ p.name for p in new_picks.picks ])}"])
 
                     #Create the cost matrix row
                     if len(new_picks.picks) == 0:
@@ -190,13 +191,11 @@ class AssociatePicksDirectory:
                             cost_row[projidx] = cost
                     cost_matrix.append(cost_row)
             except csv.Error as e:
-                sys.exit('file {}, line {}: {}'.format(projects_loc, reader.line_num, e))
+                sys.exit('file {picks_loc}, line {reader.line_num}: {e}')
             printcols(picks_out)
             print(wrns+errs)        
         return picks_by_idx, cost_matrix
-    
 
-                
 
 class bcolors:
     HEADER = '\033[95m'
@@ -208,7 +207,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 def cstring(message, color):
-    return "{0}{1}{2}".format(color,message,bcolors.ENDC)
+    return f"{color}{message}{bcolors.ENDC}"
 def cprint(message, color):
     print(cstring(message,color))
 def printcols(data, color=bcolors.OKBLUE, padding=2):
